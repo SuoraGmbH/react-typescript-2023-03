554a250170f8aec99988b32776a2e7ba186d0b63
Florian Sowade <f.sowade@suora.com>
2023-03-20 15:34:22 +0100

    Remove sample app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,7 @@
 import React from "react";
-import logo from "./logo.svg";
 
-function App() {
-  return (
-    <div className="text-center bg-black text-white">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+interface Props {}
 
-export default App;
+export const App: React.FunctionComponent<Props> = () => {
+  return <div className="container mx-auto px-4 py-4"></div>;
+};
